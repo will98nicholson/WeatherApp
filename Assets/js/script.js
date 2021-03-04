0c4e42e5610dbdf105a6dc85fc34150ef1cffe66
William Nicholson <will98nicholson@gmail.com>
2021-03-04 18:04:26 -0500

    js vars

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,1 +1,5 @@
-var apiKey = "d1907e69911786cb18b6bcba4e6a1ab6"
+var apiKey = "d1907e69911786cb18b6bcba4e6a1ab6";
+var searchButton = $("#searchButton");
+var cityInput = $(".cityInput")
+
+var urlCurrent = "api.openweathermap.org/data/2.5/weather?q=" + cityInput + "&appid=" + apiKey;
