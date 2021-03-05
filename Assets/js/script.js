d466d091310bcba11f3111d312d56283f02a5397
William Nicholson <will98nicholson@gmail.com>
2021-03-04 19:55:03 -0500

    js functions

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,5 +1,16 @@
 var apiKey = "d1907e69911786cb18b6bcba4e6a1ab6";
 var searchButton = $("#searchButton");
-var cityInput = $(".cityInput")
+var cityInput = $(".cityInput").val();
 
-var urlCurrent = "api.openweathermap.org/data/2.5/weather?q=" + cityInput + "&appid=" + apiKey;
+var urlWeather = "api.openweathermap.org/data/2.5/weather?q=" + cityInput + "&appid=" + apiKey;
+
+
+searchButton.onclick = function () {
+    if (cityInput == "") {
+        console.log(cityInput);
+    } else {
+        $.ajax({
+            url: urlWeather,
+            method: "GET",
+        }).then(function (response) { })
+    }
