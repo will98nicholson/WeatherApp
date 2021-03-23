3f3994ce907019ab919809200bf0929686ddec4d
William Nicholson <will98nicholson@gmail.com>
2021-03-23 14:25:52 -0400

    dates working

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -41,7 +41,8 @@ class UI {
 
     populateUI(data) {
         console.log(data);
-        console.log(new Date(1616519956 * 1000).toDateString())
+        console.log(data.current.dt);
+        console.log(new Date(data.current.dt * 1000).toDateString())
         this.uiContainer.innerHTML = `
         
         <div class="card mx-auto mt-5" style="width: 18rem;">
@@ -59,7 +60,7 @@ class UI {
         
         <div class="card mx-auto mt-5" style="width: 18rem;">
             <div class="card-body justify-content-center">
-                <h5 class="card-title">${new Date(1616519956 * 1000).toDateString()}</h5>
+                <h5 class="card-title">${new Date(data.daily[i].dt * 1000).toDateString()}</h5>
                 <img src="http://openweathermap.org/img/w/${data.daily[i].weather[0].icon}.png"></img>
                 <h6 class="card-subtitle mb-2 text-muted">Highs of ${data.daily[i].temp.max}. Lows of ${data.daily[i].temp.min}</h6>
                 <p class="card-text ">On ${data.name}, the current conditons are likely to be: ${data.daily[i].weather[0].description}</p>
